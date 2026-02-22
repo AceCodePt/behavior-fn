c05b271abcdbd1806c7d3ae836691475dfd21703
Sagi Carmel <sagica5273@gmail.com>
2026-02-22 15:17:35 +0200

    feat: testing

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    include: ["registry/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    globals: true,
+    environment: "jsdom",
+    browser: {
+      enabled: false,
+    },
+  },
+});
