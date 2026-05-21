433884b3bcf2f925ddbd602437e50e673e77f1e3
Sagi Carmel <sagica5273@gmail.com>
2026-05-21 09:58:34 +0300

    feat: removing input watcher in favor of compute

--- registry/behaviors/input-watcher/_behavior-definition.ts
+++
@@ -1,9 +0,0 @@
-import { uniqueBehaviorDef } from "~utils";
-import { schema } from "./schema";
-
-const definition = uniqueBehaviorDef({
-  name: "input-watcher",
-  schema,
-});
-
-export default definition;
